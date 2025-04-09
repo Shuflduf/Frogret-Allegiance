6b6ac17091b49a2114171c7a0cfe7e214b337860
Shuflduf <shuflduf@gmail.com>
2025-04-08 19:58:29 -0600

    MADE IT COOL

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="h-[50vh] flex flex-col justify-center items-center">
+  <h1 class="text-8xl w-full text-center font-medium">Welcome to SvelteKit</h1>
+  <p class="text-xl text-gray-700">
+    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+    documentation
+  </p>
+</div>
+
+<style>
+  h1 {
+    font-family: "Kanit", sans-serif;
+  }
+</style>
